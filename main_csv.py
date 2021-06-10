eb0dc528296b6eec03d3d49b162b4468ec102983
Ehtisham Sabir <Shaamdbz@gmail.com>
2021-06-10 15:20:36 +0500

    Final version

---
+++ main_csv.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import json
+from message_reader import start
+
+# setting up config file
+
+input_file = "input.csv"
+output_file = "out.csv"
+json_file = "var.json"
+
+# reading input file & sheet
+df = pd.read_csv(input_file, header=None)
+final_written = None
+
+
+def to_every_message(row):
+    global final_written
+    cell_value = row[0]
+
+    print(cell_value)
+
+    # getting response from core function
+    path_list, response_list = start()
+
+    write_list = [(cell_value, "", "")]
+    for new_row in zip(path_list, response_list):
+        write_list.append(("send", "text", new_row[0]))
+        write_list.append(("expectPayload", "equalTo", new_row[1]))
+    if final_written is None:
+        final_written = write_list
+    else:
+        final_written = final_written + write_list
+
+
+# after reading each yellow lable applying function
+df.apply(to_every_message, raw=True, axis=1)
+
+# from dataframes to output excel
+df_out = pd.DataFrame(final_written)
+df_out.to_csv(output_file, index=False, header=False)
--- message_reader.py
+++ message_reader.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import json
 
 stack_path = []
